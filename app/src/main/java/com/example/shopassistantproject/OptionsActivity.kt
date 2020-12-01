051b5dec499ee24431bae9533bbbdb1ce0ffc640
Krystian Wiewiór <krystian.wiewior@gmail.com>
2020-12-01 17:27:31 +0100

    Just Commit

--- app/src/main/java/com/example/shopassistantproject/OptionsActivity.kt
+++ app/src/main/java/com/example/shopassistantproject/OptionsActivity.kt
@@ -1,16 +1,28 @@
 package com.example.shopassistantproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.shopassistantproject.databinding.ActivityOptionsBinding
+import kotlinx.android.synthetic.main.activity_options.*
+
 
 class OptionsActivity : AppCompatActivity() {
+
+    private lateinit var sp: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //What exactly layoutinflater makes?
         val binding = ActivityOptionsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val intent1 = intent
+
+        sp = getPreferences(Context.MODE_PRIVATE)
+
+
     }
 }

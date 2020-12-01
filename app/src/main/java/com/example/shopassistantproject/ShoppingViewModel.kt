7f7793ebd77e134425bb8d8ab0df2469a7778f2f
Krystian Wiewiór <krystian.wiewior@gmail.com>
2020-12-01 13:59:57 +0100

    ShoppingViewModel added

---
+++ app/src/main/java/com/example/shopassistantproject/ShoppingViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.shopassistantproject
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+
+class ShoppingViewModel(application: Application) : AndroidViewModel(application){
+
+    private val repo: ShoppingRepo
+    val allShopping: LiveData<List<Shopping>>
+
+    init {
+        repo = ShoppingRepo(ShoppingDB.getDatabase(application.applicationContext).getShoppingDao())
+        allShopping = repo.getShopping()
+    }
+
+    fun add(shopping: Shopping) = repo.add(shopping)
+
+    fun modify(shopping: Shopping) = repo.modify(shopping)
+
+    fun remove(shopping: Shopping) = repo.delete(shopping)
+
+    fun removeAll() = repo.deleteAll()
+}
